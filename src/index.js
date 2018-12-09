dd50c173caae2fc78efe1ae43227afb59fb5ad80
Maels1991 <r.wan.guillou+github@gmail.com>
2018-12-09 19:16:46 +0100

    SIngle file react-redux counter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {connect, Provider} from 'react-redux';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const INCREMENT = 1, DECREMENT = 2;
+
+const Counter = ({count, increment, decrement}) => {
+  return (
+    <div className="counter">
+      <div>React-Redux Counter</div>
+      <div className="controls">
+        <button onClick={decrement}>-</button>
+        <div>{count}</div>
+        <button onClick={increment}>+</button>
+      </div>
+    </div>
+  );
+};
+
+
+const mapStateToProps = state => ({
+  count: state.count
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({type: INCREMENT}),
+    decrement: () => dispatch({type: DECREMENT})
+  };
+};
+
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+const initialState = {
+  count: 0
+};
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1
+      };
+    break;
+    case DECREMENT:
+      return {
+        count: state.count - 1
+      };
+    break;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedComponent />
+  </Provider>,
+  document.querySelector('#root')
+);
